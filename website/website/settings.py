7d0998e0debd09dc9ebb6c85316d91260695e806
chiraggada2911 <chiraggada67@gmail.com>
2019-03-14 09:13:15 +0530

    Email

--- website/website/settings.py
+++ website/website/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landingPage'
+    'landingPage',
+    'django.contrib.humanize',
+
 
 ]
 
@@ -131,5 +133,5 @@ EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'chiraggada67@gmail.com'
-EMAIL_HOST_PASSWORD = 'zioiqpnanhtqdpsg'
+EMAIL_HOST_USER = 'champtem11@gmail.com'
+EMAIL_HOST_PASSWORD = 'atzhaurkgtuuxwbv'
