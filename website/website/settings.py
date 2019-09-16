bfd84502b8e2f2dc419d9c7a65edce37c3b84d0f
Jatin Dalvi <jatinhdalvi@gmail.com>
2019-09-16 13:48:27 +0530

    password criteria

--- website/website/settings.py
+++ website/website/settings.py
@@ -100,13 +100,7 @@ BROKER_URL = "django://" # tell kombu to use the Django database as the message 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
